7e326e584bacf7b1a944872362c8a83627efb176
khaled-ElDeep <48034856+khaled-ElDeep@users.noreply.github.com>
2019-03-09 21:18:13 +0200

    connection

--- React/src/servics/tweetService.js
+++ React/src/servics/tweetService.js
@@ -1,7 +1,7 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
-const apiEndpoint = apiUrl + "/hashTag";
+const apiEndpoint = apiUrl + "/HashTag";
 
 function tweetUrl(id) {
   return `${apiEndpoint}/${id}`;
@@ -15,14 +15,8 @@ export function getTweet(tweetId) {
   return http.get(tweetUrl(tweetId));
 }
 
-export function sendTweet(tweet) {
-  if (tweet) {
-    const body = { ...tweet };
-
-    return http.put(apiEndpoint, body);
-  }
-
-  return http.post(apiEndpoint, tweet);
+export function sendTweet(word) {
+  return http.post(apiEndpoint, word);
 }
 
 export function deleteTweet(tweetId) {
