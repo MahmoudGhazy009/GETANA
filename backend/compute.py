bb307e904cff4951d1365520e23fd417799d01b3
MahmoudGhazy <mahmoudghazy009@gmail.com>
2019-03-10 02:28:32 +0200

    conn_python

---
+++ backend/compute.py
@@ -0,0 +1,17 @@
+import sys, json
+
+
+def main():
+    lines = sys.stdin.readlines()[0].replace('"',"").replace("\n","")
+    x = [{
+     "name": lines,
+     "age": 30,
+    "city": "New York"
+    }]
+    y=json.dumps(x)
+    #return the sum to the output stream
+    print(y)
+
+# Start process
+if __name__ == '__main__':
+    main()
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+//--------for connect with python file-------//
+let {PythonShell} = require('python-shell');
+let options = {
+  mode:"json",
+  pythonOptions: ['-u'], // get print results in real-time
+  scriptPath: '',
+  port:3001
+};
+//=======================//
+
 const app = express();
 app.use(express.json());
 
 const c = [{ id: 1, search: "l" }];
+
 app.post("/api/HashTag", (req, res) => {
+  let pyshell = new PythonShell('compute.py',options);
+
   const customer = {
     id: c.length + 1,
     search: req.body.search
@@ -11,7 +24,20 @@ app.post("/api/HashTag", (req, res) => {
   c.push(customer);
   {
     console.log(c[c.length - 1]["search"], "opopo");
-
+//=====----communicate with python---//
+    const word=c[c.length - 1]["search"];
+    pyshell.send(word); 
+    pyshell.on("message",async function (message) {
+      // received a message sent from the Python script (a simple "print" statement)
+      k= JSON.parse(JSON.stringify(message));
+      console.log(JSON.parse(JSON.stringify(message)));
+    });
+    pyshell.end(function (err) {
+      if (err){
+          throw err;
+      };
+    console.log('finished',k[0]["name"]);});
+    //------------------------------//
     res.send(c);
   }
 });
