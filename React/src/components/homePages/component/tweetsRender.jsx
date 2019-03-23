4895e0400ab88f5e68b7e800d8cce7265e86920a
khaled-ElDeep <48034856+khaled-ElDeep@users.noreply.github.com>
2019-03-23 19:01:43 +0200

    Update tweetsRender.jsx

--- React/src/components/homePages/component/tweetsRender.jsx
+++ React/src/components/homePages/component/tweetsRender.jsx
@@ -31,12 +31,11 @@ class TweetsRender extends Form {
     let clicked = true;
     this.setState(this.state.data);
     this.setState({ clicked });
-    
 
     const { data: c } = await sendTweet(this.state.data);
-   // const {data: tweets}=await getTweets();
+    // const {data: tweets}=await getTweets();
     console.log("Submittesdsdd", c.timeline[1]);
-   // console.log("here what back",tweets);
+    // console.log("here what back",tweets);
   };
 
   render() {
